34c7655caac4cd3b574ad5e556aeb1440ef7ef45
Billal GHILAS <bilal@evercam.io>
2025-05-21 17:29:39 +0100

    convert I420 to raw (rgb24)

--- lib/ex_yuv.ex
+++ lib/ex_yuv.ex
@@ -1,3 +1,37 @@
 defmodule ExYUV do
   @moduledoc File.read!("README.md")
+
+  alias ExYUV.NIF
+
+  @type width :: pos_integer()
+  @type height :: pos_integer()
+
+  @type y_plane :: binary()
+  @type u_plane :: binary()
+  @type v_plane :: binary()
+
+  @doc """
+  Converts I420 YUV format to RGB24 format.
+  """
+  @spec i420_to_raw!(binary(), width(), height()) :: binary()
+  def i420_to_raw!(data, width, height) do
+    {y_plane, u_plane, v_plane} =
+      case data do
+        [y, u, v] -> {y, u, v}
+        {y, u, v} -> {y, u, v}
+        data -> planes_from_binary(data, width, height, :I420)
+      end
+
+    NIF.i420_to_raw(y_plane, u_plane, v_plane, width, height)
+  end
+
+  defp planes_from_binary(data, width, height, :I420) when is_binary(data) do
+    y_plane_size = width * height
+    u_plane_size = div(width * height, 4)
+
+    <<y::binary-size(y_plane_size), u::binary-size(u_plane_size), v::binary-size(u_plane_size)>> =
+      data
+
+    {y, u, v}
+  end
 end
--- lib/ex_yuv/nif.ex
+++ lib/ex_yuv/nif.ex
@@ -8,4 +8,8 @@ defmodule ExYUV.NIF do
     path = :filename.join(:code.priv_dir(:ex_yuv), ~c"yuv_nif")
     :erlang.load_nif(path, 0)
   end
+
+  def i420_to_raw(_y_plane, _u_plane, _v_plane, _width, _height) do
+    :erlang.nif_error(:nif_not_loaded)
+  end
 end
