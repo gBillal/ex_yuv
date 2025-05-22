172eb44173c52ac85f3047f3a0485d9a43e45ab1
Billal Ghilas <84322223+gBillal@users.noreply.github.com>
2025-05-22 18:36:07 +0100

    Scale I420 (#3)

--- lib/ex_yuv.ex
+++ lib/ex_yuv.ex
@@ -10,7 +10,12 @@ defmodule ExYUV do
   @type u_plane :: binary()
   @type v_plane :: binary()
 
-  @type i420 :: binary() | {y_plane(), u_plane(), v_plane()} | [binary()]
+  @type i420_data :: binary() | {y_plane(), u_plane(), v_plane()} | [binary()]
+  @type yuv_planes :: {y_plane(), u_plane(), v_plane()}
+
+  @type filter_mode :: :none | :linear | :bilinear | :box
+
+  @filter_modes [:none, :linear, :bilinear, :box]
 
   @doc """
   Converts from I420 to RGB24 format.
@@ -18,24 +23,42 @@ defmodule ExYUV do
   The input should be the planes (Y, U and V), in case a binary is
   provided, it'll try to get the planes from it.
   """
-  @spec i420_to_raw!(i420(), width(), height()) :: binary()
+  @spec i420_to_raw!(i420_data(), width(), height()) :: binary()
   def i420_to_raw!(data, width, height) do
-    {y_plane, u_plane, v_plane} =
-      case data do
-        [y, u, v] -> {y, u, v}
-        {y, u, v} -> {y, u, v}
-        data -> planes_from_binary(data, width, height, :I420)
-      end
-
+    {y_plane, u_plane, v_plane} = yuv_planes(data, width, height)
     NIF.i420_to_raw(y_plane, u_plane, v_plane, width, height)
   end
 
   @doc """
   Converts from RGB24 to I420 format.
   """
-  @spec raw_to_i420!(binary(), width(), height()) :: {y_plane(), u_plane(), v_plane()}
+  @spec raw_to_i420!(binary(), width(), height()) :: yuv_planes()
   def raw_to_i420!(data, width, height) do
     NIF.raw_to_i420(data, width, height)
+  end
+
+  @doc """
+  Scale I420 image.
+
+  The following filters are supported: `none`, `linear`, `bilinear`, and `box`.
+
+  Check [libyuv](https://chromium.googlesource.com/libyuv/libyuv/+/refs/heads/main/docs/filtering.md) documentation for more details.
+  """
+  @spec scale_i420!(i420_data(), width(), height(), width(), height()) :: yuv_planes()
+  @spec scale_i420!(i420_data(), width(), height(), width(), height(), filter_mode()) ::
+          yuv_planes()
+  def scale_i420!(data, width, height, out_width, out_height, filter_mode \\ :none)
+      when filter_mode in @filter_modes do
+    {y_plane, u_plane, v_plane} = yuv_planes(data, width, height)
+    NIF.scale_i420(y_plane, u_plane, v_plane, width, height, out_width, out_height, filter_mode)
+  end
+
+  defp yuv_planes(data, width, height) do
+    case data do
+      [y, u, v] -> {y, u, v}
+      {y, u, v} -> {y, u, v}
+      data -> planes_from_binary(data, width, height, :I420)
+    end
   end
 
   defp planes_from_binary(data, width, height, :I420) when is_binary(data) do
--- lib/ex_yuv/nif.ex
+++ lib/ex_yuv/nif.ex
@@ -1,12 +1,16 @@
 defmodule ExYUV.NIF do
   @moduledoc false
 
-  @compile {:autoload, false}
-  @on_load {:load_nif, 0}
+  @on_load :load_nif
 
   def load_nif do
     path = :filename.join(:code.priv_dir(:ex_yuv), ~c"yuv_nif")
-    :erlang.load_nif(path, 0)
+
+    case :erlang.load_nif(path, 0) do
+      :ok -> :ok
+      {:error, {:reload, _}} -> :ok
+      {:error, reason} -> IO.puts("Failed to load nif: #{inspect(reason)}")
+    end
   end
 
   def i420_to_raw(_y_plane, _u_plane, _v_plane, _width, _height) do
@@ -14,6 +18,19 @@ defmodule ExYUV.NIF do
   end
 
   def raw_to_i420(_raw, _width, _height) do
+    :erlang.nif_error(:nif_not_loaded)
+  end
+
+  def scale_i420(
+        _y_plane,
+        _u_plane,
+        _v_plane,
+        _width,
+        _height,
+        _out_width,
+        _out_height,
+        _filter_mode
+      ) do
     :erlang.nif_error(:nif_not_loaded)
   end
 end
